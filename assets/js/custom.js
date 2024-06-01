47fafcf29a73b2f7d072b776d3e75883dc453594
rishii1607 <rshb2455@gmail.com>
2024-06-01 08:15:42 +0530

    add product page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,32 @@
-$(document).ready(function() {
-    $(".hamburger").click(function() {
-        $(".nav").slideToggle();
-        // $(this).toggleClass("active");
-    });
-    $(".menu-dropdown").click(function() {
-      $(this).toggleClass("active");
-      $(this).parent().closest('li').find(".submenu").slideToggle();
-   });
-    $('.banner-slider').slick({
-        arrows: false,
-        dots: true,
-      });
-      $('.product-slider').slick({
-        arrows: false,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // autoplay: true,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            }, 
-          ]
-      });
+$(document).ready(function () {
+  $(".dark-mode").click(function () {
+    $("body").toggleClass("active-mode");
+  });
+  $(".hamburger").click(function () {
+    $(".nav").slideToggle();
+  });
+  $(".menu-dropdown").click(function () {
+    $(this).toggleClass("active");
+    $(this).parent().closest("li").find(".submenu").slideToggle();
+  });
+  $(".banner-slider").slick({
+    arrows: false,
+    dots: true,
+  });
+  $(".product-slider").slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
